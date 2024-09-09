cf87e8141a058de6bfe64c173ac74aeac15693bb
Mahfuz Ahmed Zisan <mahfuz.ahmed.zisan@gmail.com>
2024-09-09 09:31:00 +0600

    Update File

--- script.js
+++ script.js
@@ -3,13 +3,11 @@ const loadLess = document.querySelector('.load-less')
 const moreCard = document.querySelector('.more-card')
 loadMore.addEventListener('click',() => {
     moreCard.style.height = moreCard.scrollHeight + 'px'
-    moreCard.style.paddingBottom = "20px"
     loadMore.style.display = "none"
     loadLess.style.display = "inline-block"
 })
 loadLess.addEventListener('click',() => {
     moreCard.style.height = '0'
-    moreCard.style.paddingBottom = "0"
     loadMore.style.display = "inline-block"
     loadLess.style.display = "none"
 })
